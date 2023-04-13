f71b8ec2e71b00eec00f987ecc91ebd316b45c56
Fithamlak <justiceofgod01@gmail.com>
2023-04-13 03:12:15 +0300

    Create codemorse.rb

---
+++ codemorse.rb
@@ -0,0 +1,53 @@
+def decode_char(morse_code)
+  morse_dict = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+  
+  morse_dict[morse_code].upcase
+end
+
+def decode_word(morse_word)
+  chars = morse_word.split(" ")
+  decoded_word = ""
+  chars.each do |char|
+    decoded_word += decode_char(char)
+  end
+  
+  decoded_word
+end
+
+def decode(morse_message)
+  words = morse_message.split("  ")
+  decode_message = ""
+  words.each do |word|
+    decode_message += decode_word(word) + " "
+  end
+  decode_message.strip
+end
+
+decode(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
