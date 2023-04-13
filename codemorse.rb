e803861c9eb73cd8d7c9200cac7fe2c549f50849
Fithamlak <justiceofgod01@gmail.com>
2023-04-13 03:33:37 +0300

    Update codemorse.rb

--- codemorse.rb
+++ codemorse.rb
@@ -1,53 +1,45 @@
-def decode_char(morse_code)
-  morse_dict = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
-  }
-  
-  morse_dict[morse_code].upcase
+MORSE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(char)
+  MORSE[char]
 end
 
-def decode_word(morse_word)
-  chars = morse_word.split(" ")
-  decoded_word = ""
-  chars.each do |char|
-    decoded_word += decode_char(char)
-  end
-  
-  decoded_word
+def decode_word(word)
+  letters = word.split
+  letters.map { |letter| decode_char(letter) }.join
 end
 
-def decode(morse_message)
-  words = morse_message.split("  ")
-  decode_message = ""
-  words.each do |word|
-    decode_message += decode_word(word) + " "
-  end
-  decode_message.strip
+def decode_message(message)
+  words = message.split('  ')
+  words.map { |word| decode_word(word) }.join(' ')
 end
 
-decode(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+# This will output: "A BOX FULL OF RUBLES".
