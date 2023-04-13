47e7513fa10ff23fb48840879b36aa163656e6f1
Fithamlak <justiceofgod01@gmail.com>
2023-04-13 03:29:28 +0300

    solve only linter with the first logical implementation

--- codemorse.rb
+++ codemorse.rb
@@ -1,45 +1,53 @@
-MORSE_CODE = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}.freeze
-
-def decode_char(char)
-  MORSE_CODE[char]
+def decode_char(morse_code)
+  morse_dict = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+  
+  morse_dict[morse_code].upcase
 end
 
-def decode_word(word)
-  letters = word.split
-  letters.map { |letter| decode_char(letter) }.join
+def decode_word(morse_word)
+  chars = morse_word.split(" ")
+  decoded_word = ""
+  chars.each do |char|
+    decoded_word += decode_char(char)
+  end
+  
+  decoded_word
 end
 
-def decode_message(message)
-  words = message.split('  ')
-  words.map { |word| decode_word(word) }.join(' ')
+def decode(morse_message)
+  words = morse_message.split("  ")
+  decode_message = ""
+  words.each do |word|
+    decode_message += decode_word(word) + " "
+  end
+  decode_message.strip
 end
 
-puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-# This will output: "A BOX FULL OF RUBLES".
+decode(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
